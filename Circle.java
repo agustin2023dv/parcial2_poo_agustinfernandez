d6970a9a156386c4fabd3a0367d12d4e8ac2c904
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2022-11-24 18:37:51 +0100

    add Agregacion metodo constructor en clase Circle + Creacion de clase Rectangulo

--- Circle.java
+++ Circle.java
@@ -2,7 +2,13 @@ public class Circle extends Shape{
 
     private double radius;
 
-    public Circle(){};
+    public Circle(){
+        this.radius = 1;
+    };
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
     public Circle(String color, boolean filled, double radius) {
         super(color, filled);
---
+++ Rectangle.java
@@ -0,0 +1,53 @@
+public class Rectangle extends Shape{
+
+    private double width, length;
+
+    public Rectangle(){
+        this.width = 1;
+        this.length= 1;
+    }
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    double getArea() {
+        return this.length*this.width;
+    }
+
+    @Override
+    double getPerimeter() {
+        return this.length*2+this.width+2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                '}';
+    }
+}
